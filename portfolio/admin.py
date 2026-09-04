8193125826b0cc5bdff1f766e3aa4cc93cdeea5b
Shashank Agasimani <shashank@Shashanks-MacBook-Air.local>
2026-09-05 00:02:59 +0530

    Updated Files

--- portfolio/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Experience, Education, Certification, Publication, Skill
-
-admin.site.register(Experience)
-admin.site.register(Education)
-admin.site.register(Certification)
-admin.site.register(Publication)
-admin.site.register(Skill)
